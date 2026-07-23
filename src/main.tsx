142b51626ebaa1a35f2eb55fa47a1dd3464386d6
peres <vinicsperes@gmail.com>
2026-07-23 18:41:42 -0300

    feature(deploy): vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App";
 import Studio from "./Studio";
@@ -7,5 +8,8 @@ import Studio from "./Studio";
 const isStudio = window.location.pathname.replace(/\/+$/, "") === "/studio";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>{isStudio ? <Studio /> : <App />}</StrictMode>,
+  <StrictMode>
+    {isStudio ? <Studio /> : <App />}
+    <Analytics />
+  </StrictMode>,
 );
